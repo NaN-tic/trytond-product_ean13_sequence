a2282a40b08ade187b4d1ed895c6672f358b8cc3
resteve <resteve@zikzakmedia.com>
2015-11-29 22:50:48 +0100

    Tests

--- tests/test_product_ean13_sequence.py
+++ tests/test_product_ean13_sequence.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
-# This file is part product_ean13_sequence module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the product_ean13_sequence module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
@@ -11,19 +10,9 @@ class ProductEAN13SequenceTestCase(ModuleTestCase):
     'Test Product EAN13 Sequence'
     module = 'product_ean13_sequence'
 
-    def setUp(self):
-        super(ProductEAN13SequenceTestCase, self).setUp()
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_ean13_sequence')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            ProductEAN13SequenceTestCase))
+        ProductEAN13SequenceTestCase))
     return suite
