48b56c88c9cde2509ac964d9693a29d8d93cc8f2
jmartin <jmartin@zikzakmedia.com>
2016-03-29 12:00:29 +0200

    Add python 3 support

--- configuration.py
+++ configuration.py
@@ -5,10 +5,10 @@ from trytond.model import fields
 from trytond.pool import PoolMeta
 
 __all__ = ['Configuration']
-__metaclass__ = PoolMeta
 
 
 class Configuration:
+    __metaclass__ = PoolMeta
     __name__ = 'product.configuration'
     ean13_sequence = fields.Many2One('ir.sequence',
         'EAN13 Sequence', required=True,
